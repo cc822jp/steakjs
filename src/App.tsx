0317693164da5b385793d7bf25a8abbdcb1989e8
Tatsuya Sasaki <sasaki@crystalcode.jp>
2020-01-27 18:20:30 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import SmokeMachine from '@bijection/smoke'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+
+    </div>
+  );
+}
+
+export default App;
